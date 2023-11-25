9596c71a97a7f8a57422f476db6f629115503518
Zsombor Szommer <zsombor.szommer@protonmail.com>
2023-11-25 15:10:13 +0100

    refactor

--- src/main.py
+++ src/main.py
@@ -31,6 +31,30 @@ from dataset_loader import (
 from visualization import display_batch_of_images
 
 
+def visualize_data(ds_train, ds_valid, ds_test):
+    print("Training:", ds_train)
+    print("Validation:", ds_valid)
+    print("Test:", ds_test)
+
+    np.set_printoptions(threshold=15, linewidth=80)
+
+    print("Training data shapes:")
+    for image, label in ds_train.take(3):
+        print(image.numpy().shape, label.numpy().shape)
+    print("Training data label examples:", label.numpy())
+
+    print("Test data shapes:")
+    for image, id_num in ds_test.take(3):
+        print(image.numpy().shape, id_num.numpy().shape)
+    print("Test data IDs:", id_num.numpy().astype("U"))  # U=unicode string
+
+    ds_iter = iter(ds_train.unbatch().batch(20))
+
+    print("PRINTING ONE BATCH OF IMAGES")
+    one_batch = next(ds_iter)
+    display_batch_of_images(one_batch)
+
+
 def main():
     print("Tensorflow version " + tf.__version__)
 
@@ -50,7 +74,8 @@ def main():
     NUM_TEST_IMAGES_512 = count_data_items(TEST_FILENAMES_512)
 
     print(
-        f"Dataset - 512 x 512: {NUM_TRAINING_IMAGES_512} training images",
+        "Dataset - 512 x 512: ",
+        f"{NUM_TRAINING_IMAGES_512} training images",
         f"{NUM_VALIDATION_IMAGES_512} validation images",
         f"{NUM_TEST_IMAGES_512} unlabelled test images",
     )
@@ -71,27 +96,7 @@ def main():
         BATCH_SIZE,
     )
 
-    print("Training - 512:", ds_train_512)
-    print("Validation - 512:", ds_valid_512)
-    print("Test - 512:", ds_test_512)
-
-    np.set_printoptions(threshold=15, linewidth=80)
-
-    print("Training - 512 data shapes:")
-    for image, label in ds_train_512.take(3):
-        print(image.numpy().shape, label.numpy().shape)
-    print("Training - 512 data label examples:", label.numpy())
-
-    print("Test - 512 data shapes:")
-    for image, id_num in ds_test_512.take(3):
-        print(image.numpy().shape, id_num.numpy().shape)
-    print("Test - 512 data IDs:", id_num.numpy().astype("U"))  # U=unicode string
-
-    ds_iter_512 = iter(ds_train_512.unbatch().batch(20))
-
-    print("PRINTING ONE BATCH OF 512 x 512 SIZE IMAGES")
-    one_batch_512 = next(ds_iter_512)
-    display_batch_of_images(one_batch_512)
+    visualize_data(ds_train_512, ds_valid_512, ds_test_512)
 
 
 if __name__ == "__main__":
