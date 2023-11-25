66a3da5fcb1be966e2fb2c9a70fe69093f5749dc
Zsombor Szommer <zsombor.szommer@protonmail.com>
2023-11-25 21:01:56 +0100

    ImageView

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,8 @@
+import "dart:io";
+
+import "package:flutter/foundation.dart";
 import "package:flutter/material.dart";
+import "package:image_picker/image_picker.dart";
 
 void main() {
   runApp(const FlowerClassificationApp());
@@ -20,8 +24,26 @@ class FlowerClassificationApp extends StatelessWidget {
   }
 }
 
-class FlowerClassificationHomePage extends StatelessWidget {
+class FlowerClassificationHomePage extends StatefulWidget {
   const FlowerClassificationHomePage({super.key});
+
+  @override
+  State<FlowerClassificationHomePage> createState() => _FlowerClassificationHomePageState();
+}
+
+class _FlowerClassificationHomePageState extends State<FlowerClassificationHomePage> {
+  XFile? _image;
+
+  Future<void> _selectImage() async {
+    final imagePicker = ImagePicker();
+    final image = await imagePicker.pickImage(source: ImageSource.gallery);
+    if (image == null) {
+      return;
+    }
+    setState(() {
+      _image = image;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +52,31 @@ class FlowerClassificationHomePage extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Flower Classification Home Page"),
       ),
-      body: const Placeholder(),
+      body: Center(
+        child: ImageView(image: _image),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async => await _selectImage(),
+        child: const Icon(Icons.add),
+      ),
     );
+  }
+}
+
+class ImageView extends StatelessWidget {
+  const ImageView({super.key, this.image});
+
+  final XFile? image;
+
+  @override
+  Widget build(BuildContext context) {
+    if (image == null) {
+      return const Text("No image selected.");
+    }
+    if (kIsWeb) {
+      return Image.network(image!.path);
+    } else {
+      return Image.file(File(image!.path));
+    }
   }
 }
