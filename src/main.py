ba9804347bd8daa8176bd08500a7a28617c56cf4
Zsombor Szommer <zsombor.szommer@protonmail.com>
2023-11-25 16:29:41 +0100

    smaller images

--- src/main.py
+++ src/main.py
@@ -67,54 +67,54 @@ def main():
     strategy = get_strategy()
 
     BATCH_SIZE = 16 * strategy.num_replicas_in_sync
-    IMAGE_SIZE_512 = [512, 512]
+    IMAGE_SIZE_192 = [192, 192]
     DS_PATH = "data"
-    PATH_512 = DS_PATH + "/tfrecords-jpeg-512x512"
+    PATH_192 = DS_PATH + "/tfrecords-jpeg-192x192"
 
-    TRAINING_FILENAMES_512 = tf.io.gfile.glob(PATH_512 + "/train/*.tfrec")
-    VALIDATION_FILENAMES_512 = tf.io.gfile.glob(PATH_512 + "/val/*.tfrec")
-    TEST_FILENAMES_512 = tf.io.gfile.glob(PATH_512 + "/test/*.tfrec")  # 100 - 10
+    TRAINING_FILENAMES_192 = tf.io.gfile.glob(PATH_192 + "/train/*.tfrec")
+    VALIDATION_FILENAMES_192 = tf.io.gfile.glob(PATH_192 + "/val/*.tfrec")
+    TEST_FILENAMES_192 = tf.io.gfile.glob(PATH_192 + "/test/*.tfrec")  # 100 - 10
 
-    NUM_TRAINING_IMAGES_512 = count_data_items(TRAINING_FILENAMES_512)
-    NUM_VALIDATION_IMAGES_512 = count_data_items(VALIDATION_FILENAMES_512)
-    NUM_TEST_IMAGES_512 = count_data_items(TEST_FILENAMES_512)
+    NUM_TRAINING_IMAGES_192 = count_data_items(TRAINING_FILENAMES_192)
+    NUM_VALIDATION_IMAGES_192 = count_data_items(VALIDATION_FILENAMES_192)
+    NUM_TEST_IMAGES_192 = count_data_items(TEST_FILENAMES_192)
 
     print(
-        "Dataset - 512 x 512: ",
-        f"{NUM_TRAINING_IMAGES_512} training images",
-        f"{NUM_VALIDATION_IMAGES_512} validation images",
-        f"{NUM_TEST_IMAGES_512} unlabelled test images",
+        "Dataset - 192 x 192: ",
+        f"{NUM_TRAINING_IMAGES_192} training images",
+        f"{NUM_VALIDATION_IMAGES_192} validation images",
+        f"{NUM_TEST_IMAGES_192} unlabelled test images",
     )
 
-    ds_train_512 = get_training_dataset(
-        TRAINING_FILENAMES_512,
-        IMAGE_SIZE_512,
+    ds_train_192 = get_training_dataset(
+        TRAINING_FILENAMES_192,
+        IMAGE_SIZE_192,
         BATCH_SIZE,
     )
-    ds_valid_512 = get_validation_dataset(
-        VALIDATION_FILENAMES_512,
-        IMAGE_SIZE_512,
+    ds_valid_192 = get_validation_dataset(
+        VALIDATION_FILENAMES_192,
+        IMAGE_SIZE_192,
         BATCH_SIZE,
     )
-    ds_test_512 = get_test_dataset(
-        TEST_FILENAMES_512,
-        IMAGE_SIZE_512,
+    ds_test_192 = get_test_dataset(
+        TEST_FILENAMES_192,
+        IMAGE_SIZE_192,
         BATCH_SIZE,
     )
 
-    visualize_data(ds_train_512, ds_valid_512, ds_test_512)
+    visualize_data(ds_train_192, ds_valid_192, ds_test_192)
 
     with strategy.scope():
-        model_512 = create_vgg16_model(strategy, IMAGE_SIZE_512 + [3], len(CLASSES))
+        model_192 = create_vgg16_model(strategy, IMAGE_SIZE_192 + [3], len(CLASSES))
 
-    optimizer_512 = tf.keras.optimizers.Adam()
+    optimizer_192 = tf.keras.optimizers.Adam()
 
     with strategy.scope():
-        compile_model(model_512, optimizer_512)
-        model_512.summary()
+        compile_model(model_192, optimizer_192)
+        model_192.summary()
 
     EPOCHS = 16
-    STEPS_PER_EPOCH_512 = NUM_TRAINING_IMAGES_512 // BATCH_SIZE
+    STEPS_PER_EPOCH_192 = NUM_TRAINING_IMAGES_192 // BATCH_SIZE
 
     model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
         "model.h5",
@@ -128,42 +128,42 @@ def main():
         mode="max",
     )
 
-    history_512 = model_512.fit(
-        ds_train_512,
-        validation_data=ds_valid_512,
+    history_192 = model_192.fit(
+        ds_train_192,
+        validation_data=ds_valid_192,
         epochs=EPOCHS,
-        steps_per_epoch=STEPS_PER_EPOCH_512,
+        steps_per_epoch=STEPS_PER_EPOCH_192,
         callbacks=[model_checkpoint_callback, early_stopping_callback],
     )
 
     display_training_curves(
-        history_512.history["loss"],
-        history_512.history["val_loss"],
+        history_192.history["loss"],
+        history_192.history["val_loss"],
         "loss",
         211,
     )
     display_training_curves(
-        history_512.history["sparse_categorical_accuracy"],
-        history_512.history["val_sparse_categorical_accuracy"],
+        history_192.history["sparse_categorical_accuracy"],
+        history_192.history["val_sparse_categorical_accuracy"],
         "accuracy",
         212,
     )
 
     generate_confusion_matrix(
-        model_512,
-        ds_valid_512,
-        NUM_VALIDATION_IMAGES_512,
+        model_192,
+        ds_valid_192,
+        NUM_VALIDATION_IMAGES_192,
     )
 
-    dataset_512 = dataset_512.unbatch().batch(20)
-    batch_512 = iter(dataset_512)
+    dataset_192 = dataset_192.unbatch().batch(20)
+    batch_192 = iter(dataset_192)
 
-    images, labels = next(batch_512)
-    probabilities = model_512.predict(images)
+    images, labels = next(batch_192)
+    probabilities = model_192.predict(images)
     predictions = np.argmax(probabilities, axis=-1)
     display_batch_of_images((images, labels), predictions)
 
-    generate_submission(model_512, ds_test_512, "submission.csv", NUM_TEST_IMAGES_512)
+    generate_submission(model_192, ds_test_192, "submission.csv", NUM_TEST_IMAGES_192)
 
 
 if __name__ == "__main__":
