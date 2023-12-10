0eb1201ac22ad0f3a197404edefa57f690a246d0
Zsombor Szommer <zsombor.szommer@protonmail.com>
2023-12-10 18:57:18 +0100

    remove backend and frontend

--- frontend/android/app/src/main/kotlin/com/example/flower_classification/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flower_classification
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- trainer/create_environment.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python -m venv .env
-source .env/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
